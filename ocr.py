c8b2fec1129a912b9018f962de31bb48c2d30944
aldo_bedrij <wbedrij@gmail.com>
2020-10-30 20:07:31 -0300

    progress bar

--- ocr.py
+++ ocr.py
@@ -3,18 +3,27 @@
     obtain the text through ocr
 """
 
+import sys
+import os
 import video_sub_avg as video
 import sub_parser as sub
 import extract_text
 import srt
 
+import datetime
+from tqdm import tqdm
+
 def to_seconds_micro(time):
     seconds = int(time)
     micro = int((time - seconds)*1e6)
-    return (seconds, micro)
+
+    return datetime.timedelta(seconds=seconds, microseconds=micro)
 
 def create_subtitle(index, times, text):
     return srt.Subtitle(index, to_seconds_micro(times.start), to_seconds_micro(times.end), text)
+
+def usage(program):
+    print(program, 'file.srt', 'file.video')
 
 def main(argv):
     if len(argv) != 3:
@@ -24,21 +33,27 @@ def main(argv):
     subtitle_name = argv[1]
     video_name = argv[2]
     (name, extension) = os.path.splitext(subtitle_name)
+    output_file = name+'_ocr'+extension
+    print(output_file)
 
     if extension == '.srt':
         parser = sub.srt_parse(subtitle_name)
 
-    video = video.extractor(video_name, buffer_size=2)
+    videofile = video.extractor(video_name, buffer_size=2)
 
     result = []
     index = 1
-    for s in parser.obtain_times():
-        frame = video.get_average_frame(s)
-        text = extract_text.extract(frame)
+    for s in tqdm(parser.obtain_times()):
+        frame = videofile.get_average_frame(s)
+        if len(frame):
+            text = extract_text.extract(frame)
+        else:
+            text = '...'
         result.append(create_subtitle(index, s, text))
         index += 1
 
-    print(srt.compose(result))
+    with open(output_file, 'w') as salida:
+        print(srt.compose(result), file=salida)
 
 if __name__ == "__main__":
     main(sys.argv)
